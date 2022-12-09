73bd8ff2951a878e284ab254208f1509dc02512d
Raphael Panic <raphael.panic@gmx.at>
2022-12-09 15:58:28 +0100

    Improved bootstrap inclusion

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,13 @@ import vue from '@vitejs/plugin-vue'
 import crossOriginIsolation from 'vite-plugin-cross-origin-isolation'
 import * as path from "path";
 
+// import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(), 
-    crossOriginIsolation()
+    crossOriginIsolation(),
   ],
   resolve: {
     alias: {
@@ -17,7 +19,14 @@ export default defineConfig({
       '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap')
     }
   },
-  build: { target: "es2020" },
+  build: {
+    target: "es2020",
+    // rollupOptions: {
+    //   output: {
+    //     format: "module"
+    //   }
+    // }
+  },
   optimizeDeps: {
     esbuildOptions: { target: "es2020", supported: { bigint: true } },
   },
